4d23a5e5a76ac40d3b333dcf5469fa7693db4b94
Hammam Hussein <hamamhussein10@gmail.com>
2025-06-27 16:58:19 -0700

    stop error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import { lazy, Suspense } from 'react';
+import { lazy } from 'react';
 import RedirectIfAuthenticated from './components/RedirectIfAuthenticated';
-import { ErrorBoundary } from 'react-error-boundary';
-import Errorfallback from './components/Errorfallback';
-import SuspenseLoader from './animation/SuspenseLoader';
+// import { ErrorBoundary } from 'react-error-boundary';
+// import Errorfallback from './components/Errorfallback';
+// import SuspenseLoader from './animation/SuspenseLoader';
 
 // why to do this? to prevent send too match code
 const Home = lazy(() => import('./pages/Home'));
@@ -26,40 +26,40 @@ const PDFManagement = lazy(() => import('./components/PDFManagement'));
 
 function App() {
   return (
-    <ErrorBoundary
-      FallbackComponent={Errorfallback}
-      onReset={() => window.location.reload()}
-    >
-      <Suspense fallback={<SuspenseLoader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
+    // <ErrorBoundary
+    //   FallbackComponent={Errorfallback}
+    //   onReset={() => window.location.reload()}
+    // >
+    //   <Suspense fallback={<SuspenseLoader />}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
 
-            <Route element={<RequireAuth />}>
-              {/* Protected Routes */}
-              <Route index element={<Home />} />
-              <Route path="/student-info" element={<StudentInformation />} />
-              <Route path="subjects" element={<Subjects />} />
-              <Route path="/add-subject" element={<AddSubjectPage />} />
-              <Route path="//pdf-options" element={<PDFManagement />} />
-              <Route path="/upload" element={<UploadFile />} />
-              <Route path="/student-result" element={<StudentResult />} />
-              <Route path="/roles" element={<StudentRoles />} />
-              <Route path="/notifications" element={<NotificationsPage />} />
-            </Route>
+        <Route element={<RequireAuth />}>
+          {/* Protected Routes */}
+          <Route index element={<Home />} />
+          <Route path="/student-info" element={<StudentInformation />} />
+          <Route path="subjects" element={<Subjects />} />
+          <Route path="/add-subject" element={<AddSubjectPage />} />
+          <Route path="//pdf-options" element={<PDFManagement />} />
+          <Route path="/upload" element={<UploadFile />} />
+          <Route path="/student-result" element={<StudentResult />} />
+          <Route path="/roles" element={<StudentRoles />} />
+          <Route path="/notifications" element={<NotificationsPage />} />
+        </Route>
 
-            <Route element={<RedirectIfAuthenticated />}>
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/verify-code" element={<VerificationPassword />} />
-              <Route path="/reset-password" element={<ResetPasswordPage />} />
-            </Route>
+        <Route element={<RedirectIfAuthenticated />}>
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/verify-code" element={<VerificationPassword />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+        </Route>
 
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </ErrorBoundary>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+    //   </Suspense>
+    // </ErrorBoundary>
   );
 }
 
