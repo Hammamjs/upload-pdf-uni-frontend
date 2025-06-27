5ff79805f26a83cf58dcdfc008621511a1e99b26
Hammam Hussein <hamamhussein10@gmail.com>
2025-06-27 16:59:10 -0700

    stop error boundary

--- src/pages/StudentResultPage.tsx
+++ src/pages/StudentResultPage.tsx
@@ -1,10 +1,14 @@
 import { Toaster } from 'react-hot-toast';
 import StudentResult from '../components/StudentResult';
+import { Suspense } from 'react';
+import SuspenseLoader from '@/animation/SuspenseLoader';
 
 const StudentResultPage = () => {
   return (
     <div className="bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
-      <StudentResult />
+      <Suspense fallback={<SuspenseLoader />}>
+        <StudentResult />
+      </Suspense>
       <Toaster reverseOrder={false} position="top-right" />
     </div>
   );
